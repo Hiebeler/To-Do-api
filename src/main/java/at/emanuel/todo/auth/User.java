33b64e8be083a3ff54670e86d35d455111bcbb12
Hiebeler <78096107+Hiebeler@users.noreply.github.com>
2021-12-22 18:05:22 +0100

    save in database works

--- src/main/java/at/emanuel/todo/auth/User.java
+++ src/main/java/at/emanuel/todo/auth/User.java
@@ -1,4 +1,27 @@
 package at.emanuel.todo.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    private String id;
+    private String email;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
